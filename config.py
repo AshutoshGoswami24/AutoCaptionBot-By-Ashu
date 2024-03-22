8c2115c0ddd38615a63ac4d58a4252a5d39b8e07
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-22 20:19:40 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-import re
-# Set your environment variables here
-api_id = int(environ.get("API_ID", ""))
-api_hash = environ.get("API_HASH", "")
-bot_token = environ.get("BOT_TOKEN", "")
-custom_caption = environ.get("CUSTOM_CAPTION", "")
