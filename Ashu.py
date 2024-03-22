750dee32f80197542d7c141029aea68d96883a8f
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-22 19:20:23 +0530

    Update Ashu.py

--- Ashu.py
+++ Ashu.py
@@ -4,11 +4,11 @@
 # Don't Remove Credit @AshutoshGoswami24
 import pyrogram, os, asyncio
 # Don't Remove Credit @AshutoshGoswami24
-try: app_id = int(os.environ.get("app_id", "20389440"))
+try: app_id = int(os.environ.get("app_id", ""))
 except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
-try: api_hash = os.environ.get("api_hash", "a1a06a18eb9153e9dbd447cfd5da2457")
+try: api_hash = os.environ.get("api_hash", "")
 except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
-try: bot_token = os.environ.get("bot_token", "6564513574:AAH3Y97iqQjSlV5vKKZdGDUohlhpA-LeSbw")
+try: bot_token = os.environ.get("bot_token", "")
 except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
 try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
