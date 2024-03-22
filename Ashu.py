281fb8adde80ac11e8ec051161641726474896bf
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-22 19:58:23 +0530

    Update Ashu.py

--- Ashu.py
+++ Ashu.py
@@ -8,22 +8,22 @@ import asyncio
 
 # Don't Remove Credit @AshutoshGoswami24
 try:
-    app_id = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", ""))
 except Exception as app_id:
     print(f"⚠️ App ID Invalid {app_id}")
 
 try:
-    api_hash = os.environ.get("API_HASH", "").upper()
+    API_HASH = os.environ.get("API_HASH", "").upper()
 except Exception as api_id:
     print(f"⚠️ Api Hash Invalid {api_hash}")
 
 try:
-    bot_token = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 except Exception as bot_token:
     print(f"⚠️ Bot Token Invalid {bot_token}")
 
 try:
-    custom_caption = os.environ.get("CUSTOM_CAPTION", "")
+    CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "")
 except Exception as custom_caption:
     print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
